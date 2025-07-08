41ff16f4f70643ccb7d164be3b1a37db502e2c7a
Shakir <shakir@example.com>
2025-07-07 23:38:46 -0700

    error solved

--- web/controllers/Product_controller.js
+++ web/controllers/Product_controller.js
@@ -1,14 +1,27 @@
+import shopify from "../shopify.js";
 
 
-
-const Createproduct = (req,res)=>{
+const Createproduct = async (req, res) => {
     try {
-        
+        const collect = new shopify.rest.Collect({ session: session });
+        collect.product_id = 921728736;
+        collect.collection_id = 841564295;
+        await collect.save({
+            update: true,
+        });
+        res.status(200).json({
+            messge:"Collect created",
+            success:true
+        })
     } catch (error) {
-        
+  console.log(error);
+  res.status(500).json({
+   messge:'interver errer',
+   success:false
+  })
     }
 };
 
 
 
-export {Createproduct}
+export { Createproduct }
